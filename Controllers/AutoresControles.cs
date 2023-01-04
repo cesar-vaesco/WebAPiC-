f8d5eeb22064bad0770b67d433105faf7348e925
Cesar-Dev <cesarvargasescorcia@gmail.com>
2023-01-03 19:37:55 -0600

    Creando controlador Get con datos harcodeados

---
+++ Controllers/AutoresControles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresControles : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            { 
+                new Autor(){  Id = 1, Name= "César Vargas" },
+                new Autor(){  Id = 2, Name= "Vero Cortez" },
+                new Autor(){  Id = 3, Name= "Glo Cortez" }
+            };
+        }
+    }
+}
