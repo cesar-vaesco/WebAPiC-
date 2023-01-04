187023629848c8bdf0fcf3d4c4f0e538e720e8bc
Cesar-Dev <cesarvargasescorcia@gmail.com>
2023-01-04 11:21:50 -0600

    Métodos put y delete controller Autores

--- Controllers/AutoresControles.cs
+++ Controllers/AutoresControles.cs
@@ -23,9 +23,43 @@ namespace WebApiAutores.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(Autor autor) 
+        public async Task<ActionResult> Post(Autor autor)
         {
-           context.Add(autor);
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
             await context.SaveChangesAsync();
             return Ok();
         }
