908dfe758c1157b1a32ba04d06f9adcc6d4bb1d9
Cesar-Dev <cesarvargasescorcia@gmail.com>
2023-01-08 15:47:55 -0600

    Validaciones personalizadas por atributo

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
@@ -10,6 +11,7 @@ namespace WebApiAutores.Entidades
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} de debe de tener más de {1} carácteres")]
+        [PrimeraLetraMayuscula]
         public string Name { get; set; }
 
         [Range(18, 120)]
@@ -17,6 +19,7 @@ namespace WebApiAutores.Entidades
         
         public int edad { get; set; }
         [CreditCard]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         [System.ComponentModel.DataAnnotations.Schema.NotMapped]
         public string TarjetaCredito { get; set; }
 
---
+++ Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace WebApiAutores.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var primeraLetra = value.ToString()[0].ToString();
+
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe de ser mayúscula");
+            }
+
+            return ValidationResult.Success;
+
+        }
+
+    }
+}
+
+//Validación personalizada que para ser utilizada basta con llamar su etiqueta en el model de la entidad donde se pretende usar
